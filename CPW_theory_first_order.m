d501ca42861b81cf0a1ae4fb2cacca8af7780b5a
Sietse <sietsedeboer22@hotmail.com>
2021-11-02 10:43:17 +0100

    Added the CPW_theory part and is kinda works

---
+++ CPW_theory_first_order.m
@@ -0,0 +1,25 @@
+classdef CPW_theory_first_order
+    %Class that describes the theory of the CPW hybrid section.
+    % constructor has the following input arguments.
+    % CPW_theory_first_order(M(lenght CPW),L(Inductance in Henry))
+    
+    properties
+        M
+        L
+    end
+    
+    methods
+        function obj = CPW_theory_first_order(M,L)
+            %Constructor CPW_theory_first_order(M(lenght CPW [m]),L(Inductance [Henry]))
+            obj.M = M;
+            obj.L = L;
+        end
+        
+        function outputArg = Zin(obj,freq)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = 2.*pi.*freq.*obj.L;
+        end
+    end
+end
+
