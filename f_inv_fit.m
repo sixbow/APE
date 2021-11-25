3dd069104b7106309b903f249984abbf7e3ee244
Sietse <sietsedeboer22@hotmail.com>
2021-11-25 17:26:26 +0100

    Made f_fit_inv more stable by setting conditions on the inputs that you can input

--- f_inv_fit.m
+++ f_inv_fit.m
@@ -1,6 +1,13 @@
 function Area = f_inv_fit(a,b,F0,baseunits)
 %Inverse of the f(A)= e^{b}A^{a}
 %Yiels   f^{inv}(F_{0}) = \frac{e^{-\frac{b}{a}}}{F_{0}^{-\frac{1}{a}}}
+if nargin<3
+    error('a,b,F0 are required arguments for function to function')
+end
+if nargin<4
+    baseunits = true;
+end
+
 if baseunits == true
     Area = exp(-b/a)/(F0^(-1/a));
 elseif baseunits == false
