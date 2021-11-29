35af93efb1ef709829ca7857a8a733d67ed90770
Sietse <sietsedeboer22@hotmail.com>
2021-11-29 14:29:06 +0100

    Added theoretical line to figure 3

--- powerlaw_fit.m
+++ powerlaw_fit.m
@@ -5,16 +5,15 @@ function [f,hpower, a , b , formula_string] = powerlaw_fit(x_normal,y_normal)
 
 f = figure;
 hold on
-hpower(1) = plot(x_normal,y_normal,'linewidth',2);
+hpower(1) = plot(x_normal,y_normal,'linewidth',2); % Plotting of the original data
 log_fit = polyfit(log(x_normal), log(y_normal),1);
 %Fitting with f(x)= ax +b
 a = log_fit(1);
 b = log_fit(2);% Indices are numbered other way around because of n series polynominal
-hpower(2) = plot(x_normal, exp(b).*(x_normal.^a),'--','linewidth',2);
+hpower(2) = plot(x_normal, exp(b).*(x_normal.^a),'--','linewidth',2); % Plotting of the fitted power law
 formula_string = 'f(A) = e^{'+string(b)+'}\cdot A^{'+string(a)+'}';
 xlabel('area [m^{2}]')
 ylabel('F_{0} [Hz]')
 
-hold off 
+hold off
 end
-
