f60dc0f7cfef2fcc9fdeb834a47ee65863553bef
Sietse <sietsedeboer22@hotmail.com>
2022-01-25 23:21:32 +0100

    Added beginning of TLMM

---
+++ Functions_TLMM/tlmm_TL_ABCD.m
@@ -0,0 +1,21 @@
+function [A B ; C D] = tlmm_TL_ABCD(freq,structure)
+%tlmm_TL_ABCD(freq,structure) calculates the ABCD matrix for a transmission
+%line
+%It expects the structure to be a struct with the fields:
+% structure.eps_eff  [-]
+% structure.Z0       [Ohm]
+% structure.length   [m]
+beta = calcbeta(freq, structure.eps_eff,2.98E8);
+A = cos(beta*structure.length);
+B = 1j*structure.Z0*sin(beta*structure.length);
+
+C = 1j*(1/structure.Z0)*sin(beta(structure.length));
+D = cos(beta*structure.length);
+
+
+
+
+
+
+end
+
