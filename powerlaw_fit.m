7f3d2a79623c4d19ca156891bb056302677801d7
Sietse <sietsedeboer22@hotmail.com>
2022-03-24 11:43:01 +0100

    Improved graphs of AreaVsF0 and make them save automatic. Improved powerfit script to give possibility to not plot.

--- powerlaw_fit.m
+++ powerlaw_fit.m
@@ -1,8 +1,10 @@
-function [f,hpower, a , b , formula_string] = powerlaw_fit(x_normal,y_normal)
+function [f,hpower, a , b , formula_string] = powerlaw_fit(x_normal,y_normal,varargin)
 %Fits a line in log log space -> indicative of power law
 % Gives back handle to figure and coof a and b in the following formula
 % f(A) = exp(b)*A^(a)
+nVarargs = length(varargin);
 
+if nVarargs == 0
 f = figure;
 hold on
 hpower(1) = plot(x_normal,y_normal,'linewidth',2); % Plotting of the original data
@@ -14,6 +16,16 @@ hpower(2) = plot(x_normal, exp(b).*(x_normal.^a),'--','linewidth',2); % Plotting
 formula_string = 'f(A) = e^{'+string(b)+'}\cdot A^{'+string(a)+'}';
 xlabel('area [m^{2}]')
 ylabel('F_{0} [Hz]')
-
+legend('Sonnet 2Part data','Power law')
 hold off
+else       
+% If 'Surpressed' is given as input the plot will be surpressed.
+f = 0;
+hpower = 0;
+log_fit = polyfit(log(x_normal), log(y_normal),1);
+%Fitting with f(x)= ax +b
+a = log_fit(1);
+b = log_fit(2);% Indices are numbered other way around because of n series polynominal
+formula_string = 'f(A) = e^{'+string(b)+'}\cdot A^{'+string(a)+'}';
+end
 end
