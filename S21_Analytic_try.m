2aaaa5f09127aee466463c6c1bd07bc3a83410df
Sietse <sietsedeboer22@hotmail.com>
2022-08-27 19:28:34 +0200

    S21_Analytic_try does describe resonance frequency really well. Q not so much!

--- S21_Analytic_try.m
+++ S21_Analytic_try.m
@@ -5,9 +5,9 @@ Z_TL = 50;
 l = 0.001;
 eps_eff = 9.9;
 Z_h = 82.4;
-Cppc = PPCAreatocap(58.3^2,295,7.8,'native')
-Cc = PPCAreatocap(4^2,295,7.8,'native')
-freq_iter = linspace(5.88E9,5.89E9,100000);
+Cppc = PPCAreatocap(3521.54,295,7.8,'native')
+Cc = PPCAreatocap(5.75*4,295,7.8,'native')
+freq_iter = linspace(5.78E9-1E6,5.79E9+1E6,100000);
 Z_CKID = zeros(1,length(freq_iter));
 CKID_Zin(freq_iter(2),Z_h,eps_eff,l,Cppc,Cc)
 freq_iter(1)
@@ -19,3 +19,4 @@ S21_dB(i) = 10*log10(abs( 2/(2+(Z_TL/Z_CKID(i)))));
 end
 
 plot(freq_iter,S21_dB);
+yline(-3);
